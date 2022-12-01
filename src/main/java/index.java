3d583bb634d76c7507757eac1e45e6a9f1619d39
jingshansong <1204333218@qq.com>
2022-12-02 01:21:59 +0800

    Changes

---
+++ src/main/java/index.java
@@ -0,0 +1,11 @@
+/**
+ * @ClassName: index
+ * @Description:
+ * @Date: 2022/12/2 0:47
+ * @Author: 景山松
+ */
+public class index {
+    public static void main(String[] args) {
+        System.out.println("hello------------");
+    }
+}
