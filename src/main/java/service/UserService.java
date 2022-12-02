00e3ea1a877487c0ceb01d9627fbe1f1f27f6c3a
jingshansong <1204333218@qq.com>
2022-12-02 08:45:52 +0800

    二次测试

---
+++ src/main/java/service/UserService.java
@@ -0,0 +1,13 @@
+package service;
+
+/**
+ * @ClassName: userService
+ * @Description:
+ * @Date: 2022/12/2 8:44
+ * @Author: 景山松
+ */
+public class UserService {
+    public static void main(String[] args) {
+        System.out.println("大家好");
+    }
+}
